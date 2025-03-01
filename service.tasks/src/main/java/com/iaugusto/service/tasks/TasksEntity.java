59045c1899c6fa9e6384833c2fc4e35cf41dfb56
Igor Augusto <igoraugustobz@gmail.com>
2025-03-01 11:16:08 -0300

    :sparkles: feat: entidade para tarefas adicionada

---
+++ service.tasks/src/main/java/com/iaugusto/service/tasks/TasksEntity.java
@@ -0,0 +1,32 @@
+package com.iaugusto.service.tasks;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class TasksEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String type;
+
+    private LocalDateTime dueDate;
+
+    private boolean notified;
+}
