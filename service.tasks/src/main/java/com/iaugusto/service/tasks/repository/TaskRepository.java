fd5213001686f45d733d26a83ac7ce3485368c61
Igor Augusto <igoraugustobz@gmail.com>
2025-03-01 23:49:15 -0300

    :sparkles: feat: lógica do envio das notificações adicionada

--- service.tasks/src/main/java/com/iaugusto/service/tasks/repository/TaskRepository.java
+++ service.tasks/src/main/java/com/iaugusto/service/tasks/repository/TaskRepository.java
@@ -2,6 +2,17 @@ package com.iaugusto.service.tasks.repository;
 
 import com.iaugusto.service.tasks.model.entities.TasksEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface TaskRepository extends JpaRepository<TasksEntity, Long> {
+
+    @Query("""
+            SELECT t FROM Task t
+            WHERE t.dueDate <= :deadline
+            AND t.notified = false
+            """)
+    List<TasksEntity> findTasksDueWithinDeadline(LocalDateTime deadline);
 }
---
+++ service.tasks/src/main/java/com/iaugusto/service/tasks/service/TaskService.java
@@ -0,0 +1,32 @@
+package com.iaugusto.service.tasks.service;
+
+import com.iaugusto.service.tasks.feign.NotificationClient;
+import com.iaugusto.service.tasks.model.dto.NotificationDTO;
+import com.iaugusto.service.tasks.model.entities.TasksEntity;
+import com.iaugusto.service.tasks.repository.TaskRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class TaskService {
+
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @Autowired
+    private NotificationClient notificationClient;
+
+    public void setNotificationForDueTaks() {        LocalDateTime deadline = LocalDateTime.now().plusDays(1);
+
+        List<TasksEntity> tasks = taskRepository.findTasksDueWithinDeadline(deadline);
+
+        for (TasksEntity task : tasks) {
+            String message = "Sua tarefa " + task.getTitle() + " está prestes a vencer";
+
+            NotificationDTO request = new NotificationDTO(message, task.getEmail());
+            notificationClient.sendNotification(request);
+            task.setNotified(true);
+        }
+    }
+}
