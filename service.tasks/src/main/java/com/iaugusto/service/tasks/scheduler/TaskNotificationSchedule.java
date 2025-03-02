7a4b30594a7f996b43e5b813173a3f9b3a4aa684
Igor Augusto <igoraugustobz@gmail.com>
2025-03-02 10:06:20 -0300

    :recycle: refactor: alteração do pacote

--- service.tasks/src/main/java/com/iaugusto/service/tasks/scheduler/TaskNotificationSchedule.java
+++ service.tasks/src/main/java/com/iaugusto/service/tasks/scheduler/TaskNotificationSchedule.java
@@ -1,5 +1,6 @@
-package com.iaugusto.service.tasks.service;
+package com.iaugusto.service.tasks.scheduler;
 
+import com.iaugusto.service.tasks.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
